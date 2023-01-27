cdfc6673fa6b5043b49323addc20cca016cac33f
karguello90 <karguello90@gmail.com>
2023-01-26 20:13:16 -0500

    added functions to prompt each employee and created instances

--- index.js
+++ index.js
@@ -7,27 +7,27 @@ const inquirer = require("inquirer")
 //console.log(Kevin)
 const teamArray = []
 
-function promptManager () {
+function promptManager() {
     inquirer.prompt([
         {
             type: "input",
             name: "managerName",
-            message: "Please enter the name of the Manager"
+            message: "Please enter the name of the Manager."
         },
         {
             type: "input",
             name: "managerID",
-            message: "Please enter the ID of the Manager"
+            message: "Please enter the ID of the Manager."
         },
         {
             type: "input",
             name: "managerEmail",
-            message: "Please enter the email of the Manager",
+            message: "Please enter the email of the Manager.",
         },
         {
             type: "input",
             name: "managerOfficeNumber",
-            message: "Please enter the office number of the Manager",
+            message: "Please enter the office number of the Manager.",
         }
     ]) .then(data => {
         const instance = new Manager(data.managerName, data.managerID, data.managerEmail, data.managerOfficeNumber)
@@ -36,3 +36,87 @@ function promptManager () {
     })
 }
     promptManager()
+
+function promptEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: "Please enter the name of the Engineer."
+        },
+        {
+            type: "input",
+            name: "engineerID",
+            message: "Please enter the ID of the Engineer."
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "Please enter the email of the Engineer.",
+        },
+        {
+            type: "input",
+            name: "engineerGitHub",
+            message: "Please enter the GitHub of the Engineer.",
+        }
+    ]) .then(data => {
+        const instance = new Engineer(data.engineerName, data.engineerID, data.engineerEmail, data.engineerGitHub)
+        teamArray.push(instance)
+        console.log(teamArray)
+    })
+}
+    promptEngineer()
+
+function promptIntern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "internName",
+            message: "Please enter the name of the Intern."
+        },
+        {
+            type: "input",
+            name: "internID",
+            message: "Please enter the ID of the Intern."
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "Please enter the email of the Intern.",
+        },
+        {
+            type: "input",
+            name: "internSchool",
+            message: "Please enter the school of the Intern.",
+        }
+    ]) .then(data => {
+        const instance = new Intern(data.internName, data.internID, data.internEmail, data.internSchool)
+        teamArray.push(instance)
+        console.log(teamArray)
+    })
+}
+    promptIntern()
+
+// function promptEmployee() {
+//     inquirer.prompt([
+//         {
+//             type: "input",
+//             name: "employeeName",
+//             message: "Please enter the name of the Employee."
+//         },
+//         {
+//             type: "input",
+//             name: "employeeID",
+//             message: "Please enter the ID of the Employee."
+//         },
+//         {
+//             type: "input",
+//             name: "employeeEmail",
+//             message: "Please enter the email of the Employee.",
+//         }
+//     ]) .then(data => {
+//         const instance = new Employee(data.employeeName, data.employeeID, data.employeeEmail)
+//         teamArray.push(instance)
+//         console.log(teamArray)
+//     })
+// }
